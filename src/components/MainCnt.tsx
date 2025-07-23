63839dfd6a71d6b6be58dfdb84dd198372f5503e
okonakona <2240057@ecc.ac.jp>
2025-07-24 03:50:39 +0900

    エラー修正

--- src/components/MainCnt.tsx
+++ src/components/MainCnt.tsx
@@ -13,7 +13,7 @@ const mockItems: FoodItem[] = [
 
 
 export default function MainCnt () {
-    const [items,setItems] = useState<FoodItem[]> (mockItems)
+    const [items,_setItems] = useState<FoodItem[]> (mockItems)
     return (
         <main>
             <h2 className="text-center m-2">食べていいもの</h2>
