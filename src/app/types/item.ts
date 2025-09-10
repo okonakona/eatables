5898898fd3f57b87644d1cbfae5ce9c556312059
okonakona <2240057@ecc.ac.jp>
2025-09-10 22:14:13 +0900

    fix: コメント部分の整理

--- src/app/types/item.ts
+++ src/app/types/item.ts
@@ -2,11 +2,11 @@ export type FoodItem = {
     id: number;
     name: string;
     category: "meat" | "fish" | "fruit" | "vegetable" | "milk" | "juice" | "snack" | "other";
-    exp_points: 1 | 3; // 1か3だけ
+    exp_points: 1 | 3; 
     is_priority: boolean;
     message: string;
     family_id: number;
-    created_at: string; // ISO文字列で返ってくることが多い
+    created_at: string; 
     updated_at: string;
     delete_flag: boolean;
 };
